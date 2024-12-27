0a3011b55ba7388970448aaa181d8afa6e51ac62
divyendu <divyendua@gmail.com>
2024-12-27 00:36:31 -0800

    byte packet buffer

---
+++ src/byte_packet_buffer.rs
@@ -0,0 +1,13 @@
+pub struct BytePacketBuffer {
+    pub buf: [u8; 512],
+    pub pos: usize,
+}
+
+impl BytePacketBuffer {
+    pub fn new() -> BytePacketBuffer {
+        BytePacketBuffer {
+            buf: [0; 512],
+            pos: 0,
+        }
+    }
+}
